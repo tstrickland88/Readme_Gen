f2a3e37528b17dc015af4b928c47982823580034
tstrickland88 <77135762+tstrickland88@users.noreply.github.com>
2021-06-23 19:31:31 -0700

    added questions to GEN_ME

---
+++ GEN_ME.md
@@ -0,0 +1,31 @@
+# lskd
+
+
+## Description 
+
+l;sd
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+
+## Installation
+
+ls;kd
+
+## Usage 
+
+d;l
+
+## Credits
+
+;sldk
+
+## License
+
+apache
+
--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer"); 
 const fs = require("fs");
-const generateMarkdown = require("utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+const { Console } = require("console");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectName",
+        message: "What Is your Projects Name?"
+    }, 
+    {
+        type: "input",
+        name: "description",
+        message: "Enter decription of your Project."
+    }, 
+    {
+        type: "input",
+        name: "installation",
+        message: "What Is The Installation of Your Project?"
+    },   
+    {
+        type: "input",
+        name: "usage",
+        message: "is there something the user needs to know about your project?"
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Is anyone else working on the project?"
+    },      
+    {
+        type: "list",
+        name: "license",
+        message: "what kind of license would you like your project to have?",
+        choices:["mit", "gpl", "apache"]
+        
+    }     
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(resp=>{
+        console.log(resp) 
+        console.log(resp.projectName)
+        var mark=generateMarkdown(resp)
+        fs.writeFileSync("GEN_ME.md",mark)
+    })
+ }
 
 // Function call to initialize app
 init();
